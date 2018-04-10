819263c0eb990229c67201620887bc0e49bf8597
Dariusz Gorski <dariuszgoorski@gmail.com>
2018-04-10 13:25:09 +0200

    added toasty messages to app

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,5 +1,6 @@
 import { VehicleService } from './../../services/vehicle.service';
 import { Component, OnInit } from '@angular/core';
+import { ToastyService } from 'ng2-toasty';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -17,7 +18,8 @@ export class VehicleFormComponent implements OnInit {
   };
 
   constructor(
-    private vehicleService: VehicleService
+    private vehicleService: VehicleService,
+    private toastyService: ToastyService
     ) { }
 
   ngOnInit() {
@@ -47,7 +49,17 @@ export class VehicleFormComponent implements OnInit {
 
   submit(){
     this.vehicleService.create(this.vehicle)
-      .subscribe(x => console.log(x));
+      .subscribe(
+        x => console.log(x),
+        err => {
+          this.toastyService.error({
+            title: 'Error',
+            msg: 'An unexpected error happened',
+            theme: 'bootstrap',
+            showClose: true,
+            timeout: 5000
+          })
+        });
   }
 
 
